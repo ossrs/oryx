fd7ece3c040f917407087f45579945e4d7428a2c
winlin <winlin@vip.126.com>
2022-01-25 21:40:02 +0800

    Release v1.0.14

--- releases/releases.js
+++ releases/releases.js
@@ -3,7 +3,7 @@
 const pkg = require('./package.json');
 
 const stable = 'v1.0.10';
-const latest = 'v1.0.13';
+const latest = 'v1.0.14';
 
 // Build the version and docker image url.
 function buildVersion(q, version) {
