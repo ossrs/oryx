f4597cbf13e6e498839bb24d435d22f4fd35aae5
winlin <winlin@vip.126.com>
2022-01-25 21:56:36 +0800

    Refine version for publisher

--- auto/pub.sh
+++ auto/pub.sh
@@ -11,13 +11,14 @@ REVISION=$(echo $RELEASE|awk -F . '{print $3}')
 let NEXT=$REVISION+1
 echo "Last release is $RELEASE, revision is $REVISION, next is $NEXT"
 
-TAG="v1.0.$NEXT"
-echo "publish $TAG"
+VERSION="1.0.$NEXT"
+TAG="v$VERSION"
+echo "publish version $VERSION as tag $TAG"
 
-cat mgmt/package.json |sed "s|\"version\":.*|\"version\":\"$TAG\",|g" > tmp.json && mv tmp.json mgmt/package.json &&
-cat releases/package.json |sed "s|\"version\":.*|\"version\":\"$TAG\",|g" > tmp.json && mv tmp.json releases/package.json &&
-cat releases/releases.js |sed "s|const\ latest\ =.*|const latest = '$TAG';|g" > tmp.js && mv tmp.js releases/releases.js
-git ci -am "Release $TAG"
+cat mgmt/package.json |sed "s|\"version\":.*|\"version\":\"$VERSION\",|g" > tmp.json && mv tmp.json mgmt/package.json &&
+cat releases/package.json |sed "s|\"version\":.*|\"version\":\"$VERSION\",|g" > tmp.json && mv tmp.json releases/package.json &&
+cat releases/releases.js |sed "s|const\ latest\ =.*|const latest = '$VERSION';|g" > tmp.js && mv tmp.js releases/releases.js
+git ci -am "Release $VERSION as $TAG"
 
 git push
 git tag -d $TAG 2>/dev/null && git push origin :$TAG
