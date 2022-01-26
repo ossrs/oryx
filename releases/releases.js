90f618558fcf315ad82e698696437b0fb75f5de0
winlin <winlin@vip.126.com>
2022-01-26 14:07:00 +0800

    Update version to v1.0.31

--- releases/releases.js
+++ releases/releases.js
@@ -3,7 +3,7 @@
 const pkg = require('./package.json');
 
 const stable = 'v1.0.31';
-const latest = 'v1.0.30';
+const latest = 'v1.0.31';
 
 // Build the version and docker image url.
 function buildVersion(q, version) {
