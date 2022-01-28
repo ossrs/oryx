617d13a7ca914abb1d99f8a8b7412854b39aa3a9
winlin <winlin@vip.126.com>
2022-01-28 09:10:16 +0800

    Update version to v1.0.46

--- releases/releases.js
+++ releases/releases.js
@@ -3,7 +3,7 @@
 const pkg = require('./package.json');
 
 const stable = 'v1.0.42';
-const latest = 'v1.0.45';
+const latest = 'v1.0.46';
 
 // Build the version and docker image url.
 function buildVersion(q, version) {
