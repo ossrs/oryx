4f874ec3ad831162f8ea9e8db02174c91cfb98a6
winlin <winlin@vip.126.com>
2022-03-17 17:57:24 +0800

    Live: Add video tutorial.

--- mgmt/ui/src/pages/ScenarioLive.js
+++ mgmt/ui/src/pages/ScenarioLive.js
@@ -15,6 +15,7 @@ export default function ScenarioLive({urls}) {
     {author: '瓦全', id: 'BV1SF411t7Li'},
     {author: '王大江', id: 'BV16r4y1q7ZT'},
     {author: '骆合祥', id: 'BV16T4y1U7CN'},
+    {author: '周亮', id: 'BV1gT4y1U76d'},
   ]));
 
   return (
