f71cf347d599dc83336a856197e5dc4042185c93
winlin <winlin@vip.126.com>
2022-03-22 14:06:44 +0800

    Add base docker image for nodejs.

---
+++ nodejs/auto/pub.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REALPATH=$(realpath $0)
+WORK_DIR=$(cd $(dirname $REALPATH)/.. && pwd)
+echo "Run pub at $WORK_DIR from $0"
+cd $WORK_DIR
+
+git st |grep -q 'nothing to commit'
+if [[ $? -ne 0 ]]; then
+  echo "Failed: Please commit before release";
+  exit 1
+fi
+
+RELEASE=$(git describe --tags --abbrev=0 --match nodejs-*)
+REVISION=$(echo $RELEASE|awk -F . '{print $3}')
+let NEXT=$REVISION+1
+echo "Last release is $RELEASE, revision is $REVISION, next is $NEXT"
+
+VERSION="1.0.$NEXT"
+TAG="nodejs-v$VERSION"
+echo "publish version $VERSION as tag $TAG"
+
+git push
+git tag -d $TAG 2>/dev/null && git push origin :$TAG
+git tag $TAG
+git push origin $TAG
+
+git remote |grep -q gitee && git push gitee && git push gitee $TAG
+git remote |grep -q cloud && git push cloud && git push cloud $TAG
+
+echo "publish $TAG ok"
+echo "    https://github.com/ossrs/srs-cloud/actions?query=branch%3A$TAG"
+
