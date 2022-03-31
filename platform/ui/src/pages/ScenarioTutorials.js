7fa6e70b1bd426a9bd746a76d82c1e8ff50fe264
winlin <winlin@vip.126.com>
2022-04-01 06:48:57 +0800

    Update tutorial.

--- platform/ui/src/pages/ScenarioTutorials.js
+++ platform/ui/src/pages/ScenarioTutorials.js
@@ -11,10 +11,12 @@ function ScenarioTutorialsCn() {
   const movieTutorials = useTutorials(React.useRef([
     {author: 'SRS', id: 'BV1844y1L7dL'},
     {author: '徐光磊', id: 'BV1RS4y1G7tb'},
+    {author: '程晓龙', id: 'BV1tZ4y1R7qp'},
     {author: 'SRS', id: 'BV1Nb4y1t7ij'},
     {author: '瓦全', id: 'BV1SF411t7Li'},
     {author: '王大江', id: 'BV16r4y1q7ZT'},
     {author: '骆合祥', id: 'BV16T4y1U7CN'},
+    {author: '马景瑞', id: 'BV1c341177e7'},
     {author: '周亮', id: 'BV1gT4y1U76d'},
     {author: '崔国栋', id: 'BV1aS4y1G7iG'},
     {author: 'SRS', id: 'BV1KY411V7uc'},
