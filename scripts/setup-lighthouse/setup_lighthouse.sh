9e00bd62d9158bfe931568ce794bf77366953453
winlin <winlin@vip.126.com>
2022-04-06 14:40:14 +0800

    Refine script.

--- scripts/setup-lighthouse/setup_lighthouse.sh
+++ scripts/setup-lighthouse/setup_lighthouse.sh
@@ -54,17 +54,17 @@ if [[ $? -ne 0 ]]; then echo "Link srs-cloud failed"; exit 1; fi
 # Cache the docker images for srs-cloud to startup faster.
 systemctl start docker &&
 echo "Cache docker images from TCR Singapore" &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/srs:4 &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/lighthouse:4 &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/node:slim &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/srs-cloud:hooks-1 &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/srs-cloud:tencent-1 &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/srs-cloud:ffmpeg-1 &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/srs-cloud:platform-1 &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/prometheus &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/redis_exporter &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/node-exporter &&
-docker pull sgccr.ccs.tencentyun.com/ossrs/certbot
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/srs:4 &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/lighthouse:4 &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/node:slim &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/srs-cloud:hooks-1 &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/srs-cloud:tencent-1 &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/srs-cloud:ffmpeg-1 &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/srs-cloud:platform-1 &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/prometheus &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/redis_exporter &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/node-exporter &&
+docker pull registry.cn-hangzhou.aliyuncs.com/ossrs/certbot
 if [[ $? -ne 0 ]]; then echo "Cache docker images failed"; exit 1; fi
 
 # If install ok, the directory should exists.
