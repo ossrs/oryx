057422a2aa08cadd9ff7d5cdb5a87d011ec09cac
winlin <winlin@vip.126.com>
2022-04-12 14:19:45 +0800

    Update mgmt version to v1.0.209

--- releases/releases.js
+++ releases/releases.js
@@ -3,7 +3,7 @@
 const pkg = require('./package.json');
 
 const stable = 'v1.0.200';
-const latest = 'v1.0.208';
+const latest = 'v1.0.209';
 
 // Build the version and docker image url.
 function buildVersion(q, version) {
