ed871c6d30004bee58d7672d3026b3503028d53f
winlin <winlin@vip.126.com>
2022-05-18 16:37:49 +0800

    Tutorials: Update docs.

--- platform/ui/src/pages/ScenarioTutorials.js
+++ platform/ui/src/pages/ScenarioTutorials.js
@@ -10,7 +10,6 @@ export default function ScenarioTutorials() {
       {author: 'SRS', id: 'BV1Nb4y1t7ij'},
       {author: '瓦全', id: 'BV1SF411t7Li'},
       {author: '王大江', id: 'BV16r4y1q7ZT'},
-      {author: '骆合祥', id: 'BV16T4y1U7CN'},
       {author: '马景瑞', id: 'BV1c341177e7'},
       {author: '周亮', id: 'BV1gT4y1U76d'},
       {author: '崔国栋', id: 'BV1aS4y1G7iG'},
