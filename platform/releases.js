98c02aaae919a4a09f342cee794d02aacda90c5a
winlin <winlin@vip.126.com>
2022-07-09 12:28:54 +0800

    Record: Update stat for record.

--- platform/releases.js
+++ platform/releases.js
@@ -23,23 +23,27 @@ async function queryLatestVersion() {
   const nid = await redis.hget(keys.redis.SRS_TENCENT_LH, 'node');
   if (nid) params.nid = nid;
 
+  // Report about local Reocrd.
+  const record = await redis.hget(keys.redis.SRS_RECORD_PATTERNS, "all");
+  if (record === 'true') params.rkd = 1;
+  const recordn = await redis.hlen(keys.redis.SRS_RECORD_M3U8_METADATA);
+  if (recordn) params.rkdn = recordn;
+
   // Report about COS and resource usage.
   const cos = await redis.hget(keys.redis.SRS_TENCENT_COS, 'bucket');
   if (cos) {
     params.cos = 1;
-
-    const cosn = await redis.hlen(keys.redis.SRS_DVR_M3U8_METADATA);
-    if (cosn) params.cosn = cosn;
   }
+  const cosn = await redis.hlen(keys.redis.SRS_DVR_M3U8_METADATA);
+  if (cosn) params.cosn = cosn;
 
   // Report about VoD and resource usage.
   const vod = await redis.hget(keys.redis.SRS_TENCENT_VOD, 'storage');
   if (vod) {
     params.vod = 1;
-
-    const vodn = await redis.hlen(keys.redis.SRS_VOD_M3U8_METADATA);
-    if (vodn) params.vodn = vodn;
   }
+  const vodn = await redis.hlen(keys.redis.SRS_VOD_M3U8_METADATA);
+  if (vodn) params.vodn = vodn;
 
   // Report about FFmpeg forwarding.
   const forward = await redis.hlen(keys.redis.SRS_FORWARD_STREAM);
