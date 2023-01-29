6e9b70739975b87f546d0b23a0f1b22b90456c15
winlin <winlin@vip.126.com>
2023-01-29 14:43:54 +0800

    Refine contact.

--- platform/ui/src/pages/Contact.js
+++ platform/ui/src/pages/Contact.js
@@ -12,21 +12,7 @@ export default function Contact() {
 function ContactCn() {
   return (
     <Container>
-      <Carousel variant="dark">
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={srsCloud}
-            alt="SRS云服务器"
-          />
-          <Carousel.Caption>
-            <h5>欢迎加SRS云服务器专享微信群</h5>
-            <p>
-              欢迎加群探讨使用经验，寻求帮助，请先观看<a href='https://www.bilibili.com/video/BV1844y1L7dL/' target='_blank' rel='noreferrer'>视频入门教程</a>。<br/>
-              SRS以及音视频相关的问题，请移步相关开源社区交流。
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Carousel variant="dark" interval={null}>
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -40,21 +26,20 @@ function ContactCn() {
             </p>
           </Carousel.Caption>
         </Carousel.Item>
-        {/*
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={srsServer}
-            alt="SRS开源服务器"
+            src={srsCloud}
+            alt="SRS云服务器"
           />
           <Carousel.Caption>
-            <h5>欢迎加SRS开源服务器社区</h5>
+            <h5>欢迎加云SRS微信群</h5>
             <p>
-              SRS是全球Top1的开源视频服务器，应用在直播和WebRTC领域。
+              欢迎加群探讨使用经验，寻求帮助，请先观看<a href='https://www.bilibili.com/video/BV1844y1L7dL/' target='_blank' rel='noreferrer'>视频入门教程</a>。<br/>
+              提需求或咨询，请加SRS付费星球。
             </p>
           </Carousel.Caption>
         </Carousel.Item>
-        */}
       </Carousel>
     </Container>
   );
