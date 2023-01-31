83eff90ef4ae279179023980e95d7c636b4e5a3b
winlin <winlin@vip.126.com>
2023-01-31 13:55:22 +0800

    Fix bugs.

--- releases/main.go
+++ releases/main.go
@@ -7,7 +7,9 @@ import (
 	"os"
 )
 
-const stable = "v1.0.256"
+// We should keep the stable version as 256, because for new architecture, we don't support automatically upgrade, so
+// this feature is actually not used, but we should keep a specified version for compatibility.
+const stable = "v1.0.256";
 const latest = "v1.0.280";
 const api = "v1.0.365";
 
