12adb18141011c9daed797185677459c73402026
winlin <winlin@vip.126.com>
2023-02-18 08:47:46 +0800

    Update version to releases-v1.0.372

--- releases/version.go
+++ releases/version.go
@@ -9,4 +9,4 @@ package main
 // this feature is actually not used, but we should keep a specified version for compatibility.
 const stable = "v1.0.256"
 const latest = "v1.0.286"
-const api = "v1.0.371"
+const api = "v1.0.372"
