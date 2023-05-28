fb5c0dfb033062c06320b0d7aa831423abd273c5
winlin <winlin@vip.126.com>
2023-05-28 08:11:59 +0800

    Update mgmt version to v1.0.298

--- releases/version.go
+++ releases/version.go
@@ -8,5 +8,5 @@ package main
 // We should keep the stable version as 193, because for new architecture, we don't support automatically upgrade, so
 // this feature is actually not used, but we should keep a specified version for compatibility.
 const stable = "v1.0.193"
-const latest = "v1.0.297"
+const latest = "v1.0.298"
 const api = "v1.0.374"
