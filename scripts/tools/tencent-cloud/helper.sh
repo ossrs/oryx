145ac7fe6ee3a8c1a823edc7cbfcc73015e220ea
winlin <winlin@vip.126.com>
2023-08-23 19:00:28 +0800

    Lighthouse: Refine lighthouse script. v5.8.26

--- scripts/tools/tencent-cloud/helper.sh
+++ scripts/tools/tencent-cloud/helper.sh
@@ -6,7 +6,13 @@ SCRIPT_DIR=$(cd $(dirname ${REALPATH}) && pwd)
 WORK_DIR=$(cd $(dirname ${REALPATH}) && pwd)
 cd ${WORK_DIR}
 
-source venv/bin/activate
+if [[ ! -d venv ]]; then
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else
+    source venv/bin/activate
+fi
 ret=$?; if [[ 0 -ne ${ret} ]]; then echo "Failed to activate venv."; exit ${ret}; fi
 
 echo "Execute: python $@"
